492e4d0e5d2aec0f456cb2cee5ba044e1bbd0e04
Lucas <lucassouza2500@gmail.com>
2025-12-10 13:11:35 -0300

    corrige dark mode

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // alternar modo dark
-const btn = document.getElementById("darkModeBtn");
-btn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
+
 
 // animação de cards ao aparecer
 const cards = document.querySelectorAll(".card");
@@ -19,3 +16,11 @@ const observer = new IntersectionObserver(
 );
 
 cards.forEach((card) => observer.observe(card));
+
+const btn = document.getElementById("darkModeBtn");
+
+if (btn) {
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+}
